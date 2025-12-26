030afb370a13b2d82780fbf6ca609a0378252194
onettww <3759065@qq.com>
2025-12-26 16:58:22 +0800

    Update hook-streamlit.py

--- hooks/hook-streamlit.py
+++ hooks/hook-streamlit.py
@@ -1,2 +1,7 @@
-from PyInstaller.utils.hooks import copy_metadata
+from PyInstaller.utils.hooks import collect_all_data_files, copy_metadata
+
 datas = copy_metadata('streamlit')
+
+# 收集Streamlit的静态文件
+static_files = collect_all_data_files('streamlit')
+datas += static_files
